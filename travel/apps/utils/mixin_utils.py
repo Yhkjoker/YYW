4d31245dbefaf329756feeffdfacc0ec0f9f3f19
wutonghe <wutonghe@megvii.com>
2019-04-27 11:06:40 +0800

    mixin change

--- travel/apps/utils/mixin_utils.py
+++ travel/apps/utils/mixin_utils.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+"""类的方法和独立的函数不完全相同，所以你不可以直接将函数装饰器运用到方法上
+ —— 你首先需要将它转换成一个方法装饰器。
+"""
 
 class LoginRequiredMixin(object):
     """
