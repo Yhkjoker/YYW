6f22cacea5d8081ea81d9589bdda87c8b0eb313c
wutonghe <2320602922@qq.com>
2019-05-28 21:52:59 +0800

    修改redis

--- travel/apps/utils/redis_connect.py
+++ travel/apps/utils/redis_connect.py
@@ -3,7 +3,7 @@ import redis
 
 class RedisConnection(object):
     def __init__(self):
-        pool = redis.ConnectionPool(host='localhost', port=6379,
+        pool = redis.ConnectionPool(host='47.107.238.126', port=6379,
                                     decode_responses=True)  # host是redis主机，需要redis服务端和客户端都起着 redis默认端口是6379
         self._r = redis.Redis(connection_pool=pool)
 
